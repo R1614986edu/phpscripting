9ba72012db62c21753fa70f8182e1e50091bad3d
R1614986edu <r1614986@student.southstaffs.ac.uk>
2017-06-16 09:42:06 +0100

    Commit
    Full project added However GitHub will not interpret PHP Correctly.

---
+++ guestbook.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>
+Guestbook
+</title>
+</head>
+<body>
+<h1>Guest Book</h1>
+<?php
+$name = $_POST['name'];
+$message = $_POST['message'];
+// connect to MySQL
+$conn = @mysql_connect ( "mysql.hostinger.co.uk", "u877267001_gbook", "gbookpassword")
+or die (mysql_error());
+// select the database
+$rs = @mysql_select_db ( "u877267001_gbook", $conn ) or die (mysql_error());
+//Create the query
+$sql = "INSERT INTO message(guestName, msg) VALUES ('$name','$message')";
+// execute the query
+$rs = mysql_query( $sql, $conn ) or die (mysql_error());
+?>
+<p>Message successfully saved.</p>
+<p>Click <a href="guestbook.html">here</a> to return to Guestbook.</p>
+</body>
+</html>
---
+++ storage/error_2.php
@@ -0,0 +1,11 @@
+<?
+    //error handler
+    function customError($errno, $errstr)
+    {
+        echo "<b>Error:</b> [$errno] $errstr";
+    }
+    //set error handler
+    set_error_handler("customError");
+    //invoke the error handler
+    echo($nonvar);
+?>
